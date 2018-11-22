3787c1251cb9a2f8697cfc460456c6487211d1e4
jwbanta <34922941+jwbanta@users.noreply.github.com>
2018-11-22 01:02:17 -0600

    Course work

---
+++ app.js
@@ -0,0 +1,7 @@
+console.log("Chayt is a bunhole");
+
+// JSX - JavaScript XML
+var template = <h2>Chayt is a punk nasty!</h2>;
+var appRoot = document.getElementById('app');
+
+ReactDOM.render(template, appRoot);
